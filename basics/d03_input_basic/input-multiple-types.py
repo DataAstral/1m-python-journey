02a526253bc2dac8e73eac9d1a1b2cfa22dcd425
DataAstral <elena.ua.uk@gmail.com>
2026-03-11 22:29:32 +0000

    Add input handling for multiple data types

---
+++ basics/d03_input_basic/input-multiple-types.py
@@ -0,0 +1,17 @@
+# Ask the user to enter a number
+number = int(input("Enter a whole number: "))
+
+# Ask the user to enter a decimal number
+decimal = float(input("Enter a decimal number: "))
+
+# Ask the user to enter text
+text = input("Enter any text: ")
+
+# Ask the user to enter True or False
+boolean = bool(input("Enter True or False: "))
+
+# Show the results
+print("Integer:", number)
+print("Float:", decimal)
+print("String:", text)
+print("Boolean:", boolean)
