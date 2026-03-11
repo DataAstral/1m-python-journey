035e0df54598ae85e647dee61f3c4dd96b0eca2c
DataAstral <elena.ua.uk@gmail.com>
2026-03-11 22:18:13 +0000

    Add name and age input program

---
+++ basics/d03_input_basic/name_age_input.py
@@ -0,0 +1,11 @@
+# Welcome message
+print("Welcome to my Python program!")
+
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Ask the user for their age
+age = input("How old are you? ")
+
+# Show the result
+print("Hello", name, "you are", age, "years old.")
