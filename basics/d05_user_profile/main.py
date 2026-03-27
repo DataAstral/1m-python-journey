f77ef5b4ac18d3855fa0c54bb8e98059f7708262
DataAstral <elena.ua.uk@gmail.com>
2026-03-27 18:40:02 +0000

    Create main.py

---
+++ basics/d05_user_profile/main.py
@@ -0,0 +1,20 @@
+# Create user profile variables
+name = "Anna"          # string
+age = 25               # integer
+city = "London"        # string
+height = 1.68          # float
+is_student = True      # boolean
+
+# Print values
+print(name)
+print(age)
+print(city)
+print(height)
+print(is_student)
+
+# Print types (debugging / learning purpose)
+print(type(name))        # name
+print(type(age))         # age
+print(type(city))        # city
+print(type(height))      # height
+print(type(is_student))  # is_student
