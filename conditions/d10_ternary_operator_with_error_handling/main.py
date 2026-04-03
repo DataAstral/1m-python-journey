4dc268f4f59add3c500280500759c5621abcb458
DataAstral <elena.ua.uk@gmail.com>
2026-04-03 18:40:05 +0100

    Delete conditions/d10_ternary_operator_with_error_handling directory

--- conditions/d10_ternary_operator_with_error_handling/main.py
+++
@@ -1,7 +0,0 @@
-#Ternary operator in Python 3
-try:
-    number =int(input("Enter a number: "))
-    result = "Even" if number %2 == 0 else "Odd"
-    print(result)
-except ValueError:
-    print("Please enter a number")
