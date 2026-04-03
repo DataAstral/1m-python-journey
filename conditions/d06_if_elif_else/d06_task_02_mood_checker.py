5b39e2fb55ff3fc4432d7386e755a3dc4c7cded8
DataAstral <elena.ua.uk@gmail.com>
2026-04-03 18:41:59 +0100

    Add files via upload

---
+++ conditions/d06_if_elif_else/d06_task_02_mood_checker.py
@@ -0,0 +1,10 @@
+# Ask the user about their mood
+mood = input("What is your mood? (happy/sad/other): ")
+
+# Check the mood
+if mood == "happy":
+    print("Keep smiling!")
+elif mood == "sad":
+    print("Don't worry, be happy!")
+else:
+    print("Have a good day anyway!")
