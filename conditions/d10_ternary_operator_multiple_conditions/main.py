dca2dae103028d8ca6ce5ddce1e83ab2597f0b6c
DataAstral <elena.ua.uk@gmail.com>
2026-04-03 18:39:36 +0100

    Delete conditions/d10_ternary_operator_multiple_conditions directory

--- conditions/d10_ternary_operator_multiple_conditions/main.py
+++
@@ -1,4 +0,0 @@
-#Ternary operator in Python 5
-mood = input("What is your mood?: (happy, sad, ok): ")
-result = "happy" if mood == "happy" else "sad" if mood == "sad" else "ok"
-print(f"Your mood is {result}")
